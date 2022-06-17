febd30adbb77dabaf5662767cab5aa7a33b2c9a8
Zeddy Jeremani <zeddyemurugat@gmail.com>
2022-06-17 19:41:58 +0300

    Created initial project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { RoutingModule } from './routes/routing.module';
+import { AppComponent } from './components/root/app.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
